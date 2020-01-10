d853689cc4dd7bcad3a1646fc33c0a94854ad0ec
olive <daniellek@mobilefuse.com>
2020-01-10 16:05:36 -0500

    db files and a few folders

--- models/userscore.js
+++ models/userscore.js
@@ -1,18 +1,46 @@
-// Dependencies for various node packages
-let mysql = require("mysql");
-let inquirer = require("inquirer");
-let http = require("http");
-let fs = require("fs");
+// // Dependencies for various node packages
+// let mysql = require("mysql");
+// let inquirer = require("inquirer");
+// let http = require("http");
+// let fs = require("fs");
 
-// create connection to the sql database
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+// // create connection to the sql database
+// let connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
 
-  // DK's username (from my SQL workbench)
-  user: "root",
+//   // DK's username (from my SQL workbench)
+//   user: "root",
 
-  // PW workbench to access DB schema & seed info from mySQL
-  password: "847Fellsway",
-  database: "familyfeud_db"
-});
+//   // PW workbench to access DB schema & seed info from mySQL
+//   password: "847Fellsway",
+//   database: "familyfeud_db"
+// });
+
+//user model
+
+// module.exports = function(sequelize, DataTypes) {
+//     let Burger = sequelize.define("Burger", {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             autoIncrement: true,
+//             primaryKey: true
+//         },
+//         burger_name: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             validate: { // if empty, do not insert new record into table
+//                 len: [1]
+//             }
+//         },
+//         devoured: {
+//             type: DataTypes.BOOLEAN,
+//             defaultValue: false, // newly entered burger is automatically uneaten
+//         },
+//         date: DataTypes.DATE
+//     }, {
+//         timestamps: false // use existing date field for timestamp
+//     });
+//     Burger.sync();
+//     return Burger;
+// };
--- server.js
+++ server.js
@@ -4,21 +4,23 @@ let express = require("express");
 let bodyParser = require("body-parser");
 let methodOverride = require("method-override");
 
-let port = 3000;
+let PORT = process.env.PORT || 3000;
 
 let app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 
 // Import routes and give the server access to them.
-// let routes = require("./controllers/burgers_controller.js");
+//let routes = require("./routes/familyfeud_controller.js")(app);
+//app.use(routes);
 
-require("./routes/familyfeud_controller.js")(app);
-
-app.listen(port);
+app.listen(PORT, function(){
+    console.log('server listening on: http://localhost:' + PORT);
+});
