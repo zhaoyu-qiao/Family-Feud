8dee79dbd74b8c644f1f7a637ecf2eef7bc48ebf
Zhaoyu Qiao <qiao.zy7@gmail.com>
2020-01-11 23:40:07 -0500

    modify game.html to match game.js with variables, add display function for answer buttons, add score addition and clear up so that one single answer won't trigger multiple score addition,timer doesn't work, need to be fixed, also need to think to write user game history to API?

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-    // Structure 
+
+    // Below is the Formatted Data Structure, 
+    // Converting the Questions from database to an array of objects, 
+    // With each object as the following format
     // let q = {
     //     question: 'domdsfksdflksf',
     //     answers: [{
@@ -12,7 +15,7 @@ $(document).ready(function () {
     // }
 
 
-    // Transfer data from database to local format to be used.
+    // Function to transfer data from database to local format to be used.
     function helper(dbResponse) {
         const questionFormatted = [];
         dbResponse.forEach(function (res, index) {
@@ -30,6 +33,7 @@ $(document).ready(function () {
                 const answerObj = {
                     label: res['answer_' + id],
                     score: res['answer_score_' + id]
+
                 };
 
                 obj.answers.push(answerObj)
@@ -76,14 +80,14 @@ $(document).ready(function () {
         },
     ]
 
-    // Convert the db format into new format
+    // Convert the db format into new format using helper function
     console.log(helper(questions))
     const newQuestions = helper(questions);
 
     // global variables
     let question = $('#question');
 
-    //let answersPlaceHolder = [`$('.answer1')`, `$('.answer2')`, `$('.answer3')`, `$('.answer4')`, `$('.answer5')`, `$('.answer6')`]
+    // Link the answer html elements to js
     let answer1 = $('.answer1');
     let answer2 = $('.answer2');
     let answer3 = $('.answer3');
@@ -91,44 +95,48 @@ $(document).ready(function () {
     let answer5 = $('.answer5');
     let answer6 = $('.answer6');
 
+    // Link user guess input field with js
     let userGuess = $('#playerAnswer-input');
 
-    let timeCount = 5;
+    // Timer variables
+    let timeCount = 30;
+    let timer;
 
+    // question index starts from 0
     let currentQuestionIndex = 0;
-    let answerValues = Object.values(questions[currentQuestionIndex])
-    // console.log(answerValues);
-    let scoreOne = 0;
-    let scoreTwo = 0;
-    let scoreThree = 0;
+
+    // Define score Array for three rounds.
+    let scoreArray = [0, 0, 0];
+
 
     // Count down function
     function countDown() {
         timeCount--;
         $('#boardTimer').text(timeCount);
         if (timeCount === 0) {
-            alert("You've run out of time!")
-            timeCount = 30;
-            //timeUp();
+            //alert("You've run out of time!")
+            timesUp();
+            //timeCount = 30;
         }
     };
 
     // Timer function
-    function timer() {
-        timeCount = 30;
-        timer = setInterval(countDown, 1000);
-        if (timeCount === 0) {
-            alert("You've run out of time!")
-            clear(timer);
-        }
-    }
+    // function timer() {
+    //     timeCount = 30;
+    //     timer = setInterval(countDown, 1000);
+    //     if (timeCount === 0) {
+    //         alert("You've run out of time!")
+    //         clear(timer);
+    //     }
+    // }
 
 
 
     // Display question, hidden answers and guessed answers
     function displayQA() {
-        // Run timer
-        timer();
+        // Define variable timer using setInterval, to count down every second.
+        // !!! this doesn't work!!!
+        timer = setInterval(countDown(), 1000);
 
         // Display question and hidden answers
         question.text(questions[currentQuestionIndex].question);
@@ -152,42 +160,78 @@ $(document).ready(function () {
 
 
         let currentQuestion = newQuestions[currentQuestionIndex];
+        console.log("current QQ: ", currentQuestion);
 
         $('#add-playerAnswer').on('click', function (event) {
             event.preventDefault();
             console.log("submit is triggered");
 
+
+            // let currentQuestion = newQuestions[currentQuestionIndex];
+
+            // Deep clone answers array then change the score of the guessed answer to 0
+            // cited https://dev.to/samanthaming/how-to-deep-clone-an-array-in-javascript-3cig
+            let tempAnswers = [...currentQuestion.answers]
+            console.log("Temporary Answers", tempAnswers);
+
+
             // Loop through the answers in newQuestions[currentQuestionIndex].answers[i], 
             // Compare the answer to userGuess, then display the correct answer to the correct html element
-            // Add the score, and display the score
-            // let currentQuestion = newQuestions[currentQuestionIndex];
-            console.log(currentQuestion);
-            for (let i = 0; i < currentQuestion.answers.length; i++) {
-                if ((userGuess.val().trim()) === currentQuestion.answers[i].label) {
+            // Add the score when each answer is put in.
+            for (let i = 0; i < tempAnswers.length; i++) {
+                if ((userGuess.val().trim()) === tempAnswers[i].label) {
                     console.log("You guess right");
-                    scoreOne += currentQuestion.answers[i].score;
+                    console.log("Hi :", tempAnswers[i].score);
+                    scoreArray[currentQuestionIndex] += tempAnswers[i].score;
+                    console.log("Score current Round: ", scoreArray[currentQuestionIndex]);
 
-                    // Display the score
+                    // Display the score beside the answer
                     // Display the correct Answer
-                    console.log('Correct Answer', currentQuestion.answers[i].label);
+                    console.log('Correct Answer', tempAnswers[i].label);
                     console.log(i);
-                    $('.answer' + (i + 1)).text(currentQuestion.answers[i].label);
-
+                    $('.answer' + (i + 1)).text(tempAnswers[i].label);
+                    $('.answer_score_' + (i + 1)).text(tempAnswers[i].score);
+                    tempAnswers[i].score = 0;
                 }
             }
+            // Write scoreRound_1 to HTML
+            console.log("Current Question Number: ", currentQuestionIndex + 1);
+            $(".scoreRound_" + (currentQuestionIndex + 1)).text(scoreArray[currentQuestionIndex]);
+
+            // !!!Also needs to write into API!!!!
 
         })
-
+        timeUp();
     }
 
     displayQA();
 
     function timeUp() {
-        // Next Question or Display Results
+        clearInterval(timer);
+        $('#boardTimer').text(timeCount);
 
-        // Next Question
+        // Next Question or Display Results for a certain time
+        if (currentQuestionIndex === (newQuestions.length - 1)) {
+            // show whole result page
+            console.log("Here's your total score!")
+
+            // write into html the total score 
+            // write into API too?
+        } else {
+            // !!!show all answers and current score for a certain time!!!
+
+            // function roundResult
+
+            // display next qusetion
+            displayNextQA();
+        }
 
     }
 
-
+    function displayNextQA() {
+        timeCount = 30;
+        $('#boardTimer').text(timeCount);
+        currentQuestionIndex++;
+        displayQA();
+    }
 })
