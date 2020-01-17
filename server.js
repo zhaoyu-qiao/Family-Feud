d7a60f857079445f19d86fa7a7493890a19d1f1e
Zhaoyu Qiao <qiao.zy7@gmail.com>
2020-01-16 21:01:13 -0500

    fix the total score issue, changed the updated version of game.html

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 let methodOverride = require("method-override");
+let Sequelize = require("sequelize");
 let PORT = process.env.PORT || 3000;
 let app = express();
 // Serve static content for the app from the "public" directory in the application directory.
@@ -20,17 +21,36 @@ app.use(methodOverride("_method"));
 app.listen(PORT, function () {
     console.log('server listening on: http://localhost:' + PORT);
 });
+// app.get("/api/questions", function (req, res) {
+//     let question = require('./models/question.js');
+//     // Here we add an "include" property to our options in our findAll query
+//     // We set the value to an array of the models we want to include in a left outer join
+//     // In this case, just db.Question
+//     question.findAll({
+//         atributes: ['name'] //not sure if answers should be in their on DB 
+//     }).then(function (question) {
+//         res.json(question);
+//     });
+// });
+
 app.get("/api/questions", function (req, res) {
     let question = require('./models/question.js');
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Question
     question.findAll({
-        atributes: ['name'] //not sure if answers should be in their on DB 
-    }).then(function (question) {
-        res.json(question);
-    });
+            // attribute: 'random()',
+            order: [
+                [Sequelize.literal('RAND()')]
+            ],
+            limit: 3
+        })
+        .then((question) => {
+            res.json(question);
+        });
 });
+
+
 app.get("/api/users", function (req, res) {
     let user = require('./models/user.js');
     let userScore = require('./models/user_score.js');
@@ -47,7 +67,22 @@ app.get("/api/users", function (req, res) {
     });
 });
 
-require("./routes/html_routes")(app);
+// create api for userscore
+app.post("/api/userscore", function (req, res) {
+    let userScore = require('./models/user_score.js');
 
-// !!! That route file doesn't work yet
-// require("./routes/randomquestion")(app);
+    userScore.create(req.body).then(function (userScore) {
+        res.json(userScore);
+    });
+});
+
+// get data from api userscore
+app.get("/api/userscore", function (req, res) {
+    let userScore = require('./models/user_score.js');
+
+    userScore.findAll().then(function (dbUserScore) {
+        res.json(dbUserScore);
+    })
+})
+require("./routes/html_routes")(app);
+//require("./routes/familyfued_controlers")(app);
