54bb7dd3bca2cace2c8d64b63b704bc0dc181273
Zhaoyu Qiao <54387352+zhaoyu-qiao@users.noreply.github.com>
2020-01-17 22:26:23 -0500

    update config.js

--- config/config.js
+++ config/config.js
@@ -1,21 +1,20 @@
-module.export
-{
-    "development": {
-      "username": "root",
-      "password": "847Fellsway",
-      "database": "familyfeud_db",
-      "host": "localhost",
-      "dialect": "mysql"
-    },
-    "test": {
-      "username": "root",
-      "password": "847Fellsway",
-      "database": "database_test",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
-    },
-    "production": {
-      "use_env_variable": "process.JAWSDB_URL"
-    }
-
+module.export = {
+  "development": {
+    "username": "root",
+    "password": "SOTHISisgood1",
+    "database": "familyfeud_db",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "SOTHISisgood1",
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "process.JAWSDB_URL"
   }
+
+}
