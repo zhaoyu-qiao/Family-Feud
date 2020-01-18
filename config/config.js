c6b290d1a835082adac7c1b2ce64bdf6ba437fcd
Zhaoyu Qiao <qiao.zy7@gmail.com>
2020-01-17 21:52:24 -0500

    update api-routes with todo.js for userSuggestion page,add game css which works good

---
+++ config/config.js
@@ -0,0 +1,20 @@
+module.export = {
+  "development": {
+    "username": "root",
+    "password": "SOTHISisgood1",
+    "database": "familyfeud_db",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "SOTHISisgood1",
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "process.JAWSDB_URL"
+  }
+
+}
